7d9ee24e2803a8263cd56932a7cdcfaf5bdd4caf
Marj-11 <marj.hajyahya@gmail.com>
2020-02-01 15:50:09 +0100

    Added General Styles

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 const bodyParser = require('body-parser');
 const colors = require('colors');
 const connectDB = require('./config/db');
+const methodOverride = require('method-override');
 
 connectDB();
 
@@ -17,12 +19,14 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 // Body parser
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 app.use('/', indexRouter);
 app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server running on port ${PORT}`.yellow.bold));
